5f63d36e78c88b4e9417a276382a15eab94841dc
Brad Midgley <bmidgley@gmail.com>
2021-10-28 01:22:33 -0600

    use ts

--- main.py
+++ main.py
@@ -7,38 +7,40 @@ def ping(freq: number, message: str):
     music.rest(music.beat(BeatFraction.SIXTEENTH))
     music.set_volume(64)
     music.play_tone(freq, music.beat(BeatFraction.HALF))
-    radio.send_string(message)
+    radio.send_string("" + message + ":" + ("" + str(x)) + ":" + ("" + str(y)))
     return message
 
 def on_button_pressed_a():
     ping(1050, "ping")
-    basic.show_leds("""
-        . . . # .
-                . . . # .
-                . . . . .
-                . . . . .
-                . . . . .
-    """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def draw():
+    led.plot(2, 3)
 def initv2():
     music.set_built_in_speaker_enabled(True)
+    init()
+def init():
+    global x, y
+    radio.set_group(1)
+    x = randint(0, 100)
+    y = randint(0, 100)
+    game.score()
     music.play_tone(988, music.beat(BeatFraction.SIXTEENTH))
+    draw()
 
 def on_received_string(receivedString):
-    if receivedString == "ping":
+    global messages
+    messages = receivedString.split(":")
+    if messages[0] == "ping":
         control.wait_micros(1000 * randint(0, 1000))
         ping(1000, "pong")
 radio.on_received_string(on_received_string)
 
-basic.show_leds("""
-    # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-""")
-radio.set_group(1)
+messages: List[str] = []
+y = 0
+x = 0
+messages2 = ""
+init()
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -7,37 +7,39 @@ function ping (freq: number, message: string) {
     music.rest(music.beat(BeatFraction.Sixteenth))
     music.setVolume(64)
     music.playTone(freq, music.beat(BeatFraction.Half))
-    radio.sendString(message)
+    radio.sendString("" + (`${message}:${x}:${y}`))
     return message
 }
 input.onButtonPressed(Button.A, function () {
     ping(1050, "ping")
-    basic.showLeds(`
-        . . . # .
-        . . . # .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
 })
+function draw () {
+    led.plot(2, 3)
+}
 function initv2 () {
     music.setBuiltInSpeakerEnabled(true)
-    music.playTone(988, music.beat(BeatFraction.Sixteenth))
+    init()
+}
+function init () {
+    radio.setGroup(1)
+    x = randint(0, 100)
+    y = randint(0, 100)
+    game.score()
+music.playTone(988, music.beat(BeatFraction.Sixteenth))
+    draw()
 }
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "ping") {
+    messages = receivedString.split(":")
+    if (messages[0] == "ping") {
         control.waitMicros(1000 * randint(0, 1000))
         ping(1000, "pong")
     }
 })
-basic.showLeds(`
-    # . . . #
-    . # . # .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-radio.setGroup(1)
+let messages: string[] = []
+let messages2 = ""
+let x = 0
+let y = 0
+init()
 basic.forever(function () {
 	
 })
